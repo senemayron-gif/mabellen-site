cff675b4531e4fce8d98fdaa3c73b48deba50dce
Mabellen <senemayron@gmail.com>
2026-04-29 16:15:33 -0300

    feat: configuração para instalar app no celular

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,2 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { ReactNode } from 'react'
-
-export const metadata: Metadata = {
-  title: 'Mabellen',
-  description: 'Loja virtual',
-  manifest: '/manifest.json',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
-  return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
-  )
-}
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#c9a96e" />
