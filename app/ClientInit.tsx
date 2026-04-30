79434db5e13e7c917115084e5385bb8914492f2d
Mabellen <senemayron@gmail.com>
2026-04-30 14:46:22 -0300

    Corrigindo erro de inicialização do OneSignal

--- app/ClientInit.tsx
+++ app/ClientInit.tsx
@@ -1,14 +1,24 @@
-'use client'; // Aqui pode usar sem medo
+'use client'; // Continua sendo de cliente
 
 import { useEffect } from 'react';
 import OneSignal from 'react-onesignal';
 
 export default function ClientInit() {
   useEffect(() => {
+    // Nova forma corrigida de inicializar o OneSignal
     OneSignal.init({
       appId: "a3fa67c1-8318-4252-9fea-319342e41628",
-      notifyButton: { enable: true },
       allowLocalhostAsSecureOrigin: true,
+    }).then(() => {
+        // Depois que inicializou, mostramos o botão de notificação (o sino)
+        OneSignal.Slidedown.promptTrigger({
+            type: "category",
+            text: {
+                actionMessage: "Gostaria de receber novidades e ofertas da Mabellen?",
+                acceptButton: "Sim!",
+                cancelButton: "Não, obrigado."
+            }
+        });
     });
   }, []);
 
